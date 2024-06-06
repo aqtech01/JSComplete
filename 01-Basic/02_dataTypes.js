0657d0a5262434d03980c94c1c2b515b087d22df
Abdul Qadeer <abdulqadeer0120@gmail.com>
2024-06-06 21:57:22 +0000

    type conversion

--- 01-Basic/02_dataTypes.js
+++ 01-Basic/02_dataTypes.js
@@ -1,7 +1,40 @@
-"use strict"; // treat js as a new version
+// "use strict"; // treat js as a new version
 
-// alert("hello");  it can be run because we are using node
+// // alert("hello");  it can be run because we are using node
 
 
-console.log("Qadeer");
+// console.log("Qadeer");
+
+// // data types
+
+// // String
+
+// let name = "Qadeer";
+// console.log(name);
+// // Number
+// let age = 23;
+// console.log(age);
+// // Boolean
+// let is_loggedin = false;
+// console.log(is_loggedin);
+
+// // null 
+// let a = null;
+// console.log(a);
+
+// // undefined
+// let abc;
+// console.log(abc);
+
+// // symbol => unique
+
+// // object
+
+// console.log(typeof(a));
+
+
+
+
+// Data Type Conversion
+
 
