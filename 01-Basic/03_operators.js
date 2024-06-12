78f7baeaa1834323c64857128944a0f87a1d1421
Abdul Qadeer <abdulqadeer0120@gmail.com>
2024-06-12 17:10:24 +0000

    operators end

---
+++ 01-Basic/03_operators.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// logical operators
+
+// console.log(2>1);
+// console.log(1<2);
+// console.log(1==1);
+// console.log(2<=1);
+// console.log(2>=1);
+// console.log(2!=1);
+
+
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+
+// Strict check 
+
+
