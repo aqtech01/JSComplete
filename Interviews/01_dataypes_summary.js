f636734a24ca733ec34b8c56a31e09b13ad543a6
Abdul Qadeer <abdulqadeer0120@gmail.com>
2024-06-12 17:31:12 +0000

    data types summat

---
+++ Interviews/01_dataypes_summary.js
@@ -0,0 +1,50 @@
+"use strict";
+
+// Primitive Data Types
+    // String
+    // Number
+    // Boolean 
+    // null 
+    // undefined
+    // BigInt
+    // Symbol  // to give special value 
+
+
+    // const score = 100;
+    // console.log(typeof score);
+    // const scoreValue= 100.3;
+    // console.log(typeof scoreValue);
+    // const is_loggedin = false;
+    // console.log(typeof is_loggedin);
+    // const outsideTemp = null;
+    // console.log(typeof outsideTemp);
+    // let userEmail;
+    // console.log(typeof userEmail);
+
+    // const id = Symbol("123");
+    // console.log(typeof id);
+    // const value = 12323213213213123213123123123213213213213213n;
+    // console.log(typeof value);
+
+
+// Non Primitive Data types/ Refrence Type
+    // Arrays
+            // const heros = ["Captian America", "Iron Man", "Spider Man"]
+            // console.log(typeof heros);
+    // Objects
+    // let obj = {
+    //     name:"AQ",
+    //     age :24,
+        
+    // }
+    // console.log(typeof obj)
+
+    
+    // Functions
+
+
+    // const getScore = function (){
+    //     console.log("Hello Sparta!");
+    // }
+    // console.log(typeof getScore);
+
