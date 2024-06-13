d6e7a311c137f49bbddbeab99d09e3db397bf781
AQTECH <abdulqadeer0120@gmail.com>
2024-06-13 14:37:53 +0100

    strings methods stack and heap

---
+++ 01-Basic/04_stack_heap.js
@@ -0,0 +1,28 @@
+"use strict";
+
+
+// Stack and heap
+
+// Premitive goes to stack and give copy for print
+// let myname = "AQ";
+// let myotherName = myname;
+// myotherName = "Khawaja"
+// console.log(myotherName);
+// console.log(myname);
+
+
+
+// non premitive goes heap and give refrence
+
+
+// let user = {
+//     email: "user@gmail.com",
+//     upi: "user@upi",
+// };
+// let user2= user;
+
+// user2.email = "abdulqadeer@gmail.com";
+// console.log(user.email);
+// console.log(user2.email);
+
+
---
+++ 01-Basic/string.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// Strings
+
+// const name = "Qadeer ";
+
+// const repocount = 50;
+
+// console.log(name + repocount+ " Something");
+
+// console.log(`Hello ${name} my repo count ${repocount} and something`);
+
+// const gameName = new String("    Qadeer   ")
+
+// console.log(gameName);
+
+// console.log(gameName.slice(1,6));
+
+// console.log(gameName.substring(0,5));
+
+// console.log(gameName.slice(-8,4));
+
+const url = "hitechper%20son20";
+
+// console.log(url.includes("er"));
+
+console.log(url.split("%"));
+
+// console.log( url.replace("%20","-"));
+
+// console.log(gameName.trim());
+
+// String Functions
+
+
+// console.log(typeof gameName);
+
+// console.log( gameName.length);
+
+// console.log( gameName.indexOf("Q"));
+
+// console.log( gameName.toUpperCase());
+
+// console.log( gameName.charAt(2));
+
+// console.log( gameName.toUpperCase());
+
+
+// Strings Methods
+
+
+
