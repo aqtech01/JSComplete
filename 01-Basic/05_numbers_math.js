d75f64e81118a45362eb69bb64c45ccbf9b759a4
AQTECH <abdulqadeer0120@gmail.com>
2024-06-13 18:05:30 +0100

    Math and Numbers

---
+++ 01-Basic/05_numbers_math.js
@@ -0,0 +1,32 @@
+"use strict";
+// Numbers  
+
+// const score = 20;
+// console.log(typeof score);
+//  const balance = new Number(1000);
+//  console.log(balance);
+//  console.log(typeof balance);
+//  console.log(balance.toFixed(2));
+//  console.log(typeof balance.toString());
+//  const balance2= 123.4433423;
+//  console.log(balance2.toPrecision(4));
+
+//  const hundreds = 1000000;
+// console.log(hundreds.toLocaleString("en-IN"));
+
+//********************************Maths ************************/
+
+
+
+// console.log(Math.PI);
+// console.log(Math.abs(-444));
+// console.log(Math.round(123.533));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.sqrt(4));
+console.log(Math.random(1));
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random(min-max)*10+1));
--- practise/strings.js
+++ practise/strings.js
@@ -1,12 +1,12 @@
-"use strict";
+// "use strict";
 
-const name = "Abdul Qadeer";
-console.log(`the length is ${name.length}`);
-console.log(`Upper case ${name.toUpperCase()}`);
-console.log(`Lower Case ${name.toLowerCase()}`);
-console.log(`Find Index of Specific Character ${name.indexOf("Q")}`);
-console.log(`Find The value on given Index ${name.charAt(6)}`);
-console.log( `Use of Trim function ${name.trim()} endtrim ${name.trimStart()} startTrim ${name.trimEnd()}`);
+// const name = "Abdul Qadeer";
+// console.log(`the length is ${name.length}`);
+// console.log(`Upper case ${name.toUpperCase()}`);
+// console.log(`Lower Case ${name.toLowerCase()}`);
+// console.log(`Find Index of Specific Character ${name.indexOf("Q")}`);
+// console.log(`Find The value on given Index ${name.charAt(6)}`);
+// console.log( `Use of Trim function ${name.trim()} endtrim ${name.trimStart()} startTrim ${name.trimEnd()}`);
 
-console.log(`use Slice Function ${name.slice(1,4)}`);
-console.log(`use sub String Function ${name.substring(0,5)}`);
+// console.log(`use Slice Function ${name.slice(1,4)}`);
+// console.log(`use sub String Function ${name.substring(0,5)}`);
