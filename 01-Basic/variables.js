eff353faf2ee086b6f3528d669eba16b6da6662c
aqtech <abdulqadeer0120@gmail.com>
2024-07-01 15:31:40 +0100

    start

---
+++ 01-Basic/variables.js
@@ -0,0 +1,30 @@
+"use strict";
+
+
+
+const name = "Abdul Qadeer";
+console.log(name);
+
+// Const can't be change or declare
+
+let num = 32;
+
+console.log(num);
+// let can change but not declares
+
+num = 33;
+console.log(num);
+
+
+// var can be change or decalre
+var isDrinking = true;
+console.log(isDrinking);
+
+
+console.table()
+
+
+
+
+
+
