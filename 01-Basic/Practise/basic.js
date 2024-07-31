063954a4a80cb283d49a9d698fc543f2a0b5568c
aqtech <abdulqadeer0120@gmail.com>
2024-07-31 12:15:17 +0100

    varibales topic completed

---
+++ 01-Basic/Practise/basic.js
@@ -0,0 +1,19 @@
+// const pen = {
+//     name : "Parker Jotter Standart CT Ball Pen",
+//     color : "Black",
+//     rating : 5,
+//     isDeal : true,
+//     price:270,
+
+// }
+
+const profile = {
+  name: "Abudl Qadeer",
+  isFollow: true,
+  messages: "message",
+  posts: 195,
+  followers: 1959889,
+};
+
+console.log(profile.name);
+console.log(profile["isFollow"]);
