dd07410d9a5a4759a0cd4df0a61e30a9f652c64d
aqtech <abdulqadeer0120@gmail.com>
2024-07-31 17:53:24 +0100

    Day 2 End

---
+++ 01-Basic/loops_string.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Loops
+
+// For loops
+
+// for (let index = 0; index < 10; index++) {
+//   console.log(index + " This is Sparta!");
+// }
+
+// Calcultate sum of n number
+// let sum = 0;
+// for (let index = 0; index <= 5; index++) {
+//   sum = sum + index;
+// }
+// console.log(sum);
