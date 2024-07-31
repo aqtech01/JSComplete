a26ad0d205dc5280c5d838898d2bcf577eca7a3c
aqtech <abdulqadeer0120@gmail.com>
2024-08-01 00:38:09 +0100

    Strings Complete

---
+++ 01-Basic/Practise/strings.js
@@ -0,0 +1,8 @@
+"use strict";
+let userName = prompt("Enter Your Name!");
+
+// let firstUserName = userName.slice(0,1);
+// let
+
+userName = `Your userName is \@${userName}${userName.length}`;
+console.log(userName);
