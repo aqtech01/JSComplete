6d65a5861d99646bd09b89fdae0edbb4132bfa0b
aqtech <abdulqadeer0120@gmail.com>
2024-08-01 13:00:27 +0100

    Arrays Section Completed

--- 01-Basic/Practise/array.js
+++ 01-Basic/Practise/array.js
@@ -7,10 +7,18 @@
 // console.log(marks.length);
 // console.log(marksSum / marks.length);
 
-let itemsPrice = [250, 645, 300, 900, 50];
+// let itemsPrice = [250, 645, 300, 900, 50];
 
-let finalArray = [];
-for (let finalPrice of itemsPrice) {
-  finalArray.push(finalPrice * 0.9);
-}
-console.log(finalArray);
+// let finalArray = [];
+// for (let finalPrice of itemsPrice) {
+//   finalArray.push(finalPrice * 0.9);
+// }
+// console.log(finalArray);
+
+let comapnies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+// comapnies.shift();
+// console.log(comapnies);
+// comapnies.splice(1, 1, "Ola");
+// console.log(comapnies);
+comapnies.push("Amazon");
+console.log(comapnies);
