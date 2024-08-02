f7fcdde9bd147d1d92c25373523b6e3797895838
aqtech <abdulqadeer0120@gmail.com>
2024-08-02 09:24:55 +0100

    Functions

--- 01-Basic/functions.js
+++ 01-Basic/functions.js
@@ -1,1 +1,22 @@
 "use strict";
+// Functions in Js
+
+function myFunction() {
+  //   let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  //   console.log(arr.concat([11, 12, 13, 14, 15]));
+  //   arr.push(11);
+  //   console.log(arr);
+  //   arr.splice(9, 1, 12);
+  //   console.log(arr);
+
+  let name = "This is Sparta!";
+  //   console.log(name.endsWith("!"));
+  //   console.log(name.slice(1, 3));
+  //   console.log(name.slice(1, 3));
+  //   console.log(name.toLowerCase());
+  //   console.log(name);
+  //   console.log(name.replace("Sparta", "Abdul"));
+  //   console.log(name.trim());
+}
+
+myFunction();
