2651407e76200a75bdd7c1b8b1f78cc768f19c26
aqtech <abdulqadeer0120@gmail.com>
2024-08-03 13:37:42 +0100

    a

---
+++ Events/script.js
@@ -0,0 +1,16 @@
+"use strict";
+// let btn1 = document.querySelector(".btn_firstClick");
+
+// btn1.onclick = () => {
+//   btn1.style.color = "white";
+//   btn1.style.backgroundColor = "red";
+// };
+// let div = document.querySelector("#div1");
+// div.onmouseover = (e) => {
+//   console.log(e);
+//   console.log(e.type);
+
+//   div.style.color = "white";
+//   div.style.backgroundColor = "red";
+//   div.innerText = "Mouse Over";
+// };
