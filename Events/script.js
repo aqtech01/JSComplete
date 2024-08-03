e44513a925a6f869e6dbbd711405053214c5a7ae
aqtech <abdulqadeer0120@gmail.com>
2024-08-03 13:57:15 +0100

    events complete

--- Events/script.js
+++ Events/script.js
@@ -1,5 +1,55 @@
 "use strict";
+
+let togglebtn = document.querySelector("#toggleButton");
+
+let currentMode = "black";
+
+togglebtn.addEventListener("click", () => {
+  if (currentMode === "light") {
+    let body = document.querySelector("body");
+    currentMode = "black";
+    body.style.backgroundColor = "black";
+    togglebtn.innerText = "Black Mode";
+  } else {
+    currentMode = "light";
+    let body = document.querySelector("body");
+    body.style.backgroundColor = "white";
+    togglebtn.innerText = "White Mode";
+  }
+});
+
 // let btn1 = document.querySelector(".btn_firstClick");
+// btn1.addEventListener("click", (e) => {
+//   console.log(e.type);
+
+//   btn1.style.color = "white";
+//   btn1.style.backgroundColor = "red";
+// });
+
+// let btn2 = document.querySelector(".btn_firstClick");
+// btn1.addEventListener("click", (e) => {
+//   console.log(e.type);
+
+//   btn1.style.color = "white";
+//   btn1.style.backgroundColor = "red";
+// });
+
+// let btn3 = document.querySelector(".btn_firstClick");
+// btn1.removeEventListener("click", (e) => {
+//   console.log(e.type);
+
+//   btn1.style.color = "white";
+//   btn1.style.backgroundColor = "red";
+//   console.log("Events Removed");
+// });
+
+// let btn4 = document.querySelector(".btn_firstClick");
+// btn1.addEventListener("click", (e) => {
+//   console.log(e.type);
+
+//   btn1.style.color = "white";
+//   btn1.style.backgroundColor = "red";
+// });
 
 // btn1.onclick = () => {
 //   btn1.style.color = "white";
