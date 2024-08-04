f658c35d712fe7afee891f685766f72d04e8bdcf
aqtech <abdulqadeer0120@gmail.com>
2024-08-04 13:38:56 +0100

    tic toc toe game developing

---
+++ Projects/TicTacToe/script.js
@@ -0,0 +1,29 @@
+"use strict";
+let btn = document.querySelectorAll(".box");
+let resest_btn = document.querySelector("reset");
+// turn track  Player o
+let turnO = true;
+// Store Winning Patterns
+const winPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+btn.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disbaled = true;
+  });
+});
